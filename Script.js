eeb62f85239198fc80697f6b2060bf024b0e3b37
Adrxn2000 <amajavu@dvtsoftware.com>
2025-01-15 10:06:15 +0200

    working now

--- Script.js
+++ Script.js
@@ -1,11 +1,42 @@
-function updateClock() {
+
+function updateClock(){
     var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-    var timeString = "" + ((hours > 12) ? hours - 12 : hours);
-    timeString  += ((minutes < 10) ? ":0" : ":") + minutes;
-    timeString  += ((seconds < 10) ? ":0" : ":") + seconds;
-    timeString  += (hours >= 12) ? " P.M." : " A.M.";
-    document.getElementById("clock").firstChild.nodeValue = timeString;
+    var dname = now.getDay(),
+    mo = now.getMonth(),    
+    dnum = now.getDate(),
+    yr = now.getFullYear(),
+    h = now.getHours(),
+    m = now.getMinutes(),
+    s = now.getSeconds(),
+    pe = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+    if(h > 12){
+        h = h - 12;
+        pe = "PM";
+    }
+
+    Number.prototype.pad = function(digits){
+        for(var n = this.toString(); n.length < digits; n = 0 + n);
+        return n;
+    }
+
+
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids = ["day", "month", "daynum", "year", "hour", "minute", "second", "period"];
+    var values = [week[dname], months[mo], dnum.pad(2), yr, h.pad(2), m.pad(2), s.pad(2), pe];
+    for(var i=0; i<ids.length; i++)
+
+    document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
+
+function showTime(){
+    updateClock();
+     window.setInterval("updateClock()", 1);
+ 
+ }
+ 
