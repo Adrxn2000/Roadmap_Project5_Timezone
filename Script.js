724f153f4dad91e3da1c8ab10ea05ec12e064b10
Adrxn2000 <amajavu@dvtsoftware.com>
2025-01-15 17:06:25 +0200

    Added npm

--- Script.js
+++ Script.js
@@ -1,5 +1,57 @@
 
-function updateClock(){
+// import dayjs from "dayjs";
+// import utc from "dayjs/plugin/utc.js";
+// import timezone from "dayjs/plugin/timezone.js";
+dayjs.extend(window.dayjs_plugin_utc);  
+dayjs.extend(window.dayjs_plugin_timezone); 
+
+function Dropdown() {
+    console.log("Dropdown");
+    
+    const selector = document.querySelector(".timezone-selector");
+    selector.classList.toggle("open");
+};
+document.getElementById("dropdown").addEventListener("click", Dropdown);
+// Toggle the dropdown visibility
+let selectedTimezone 
+// Update the time when a timezone is selected
+window.updateSelectedTimezone = () => {
+     selectedTimezone = document.getElementById("timezone-select").value;
+    updateTime(selectedTimezone); // Update the displayed time
+    document.querySelector(".timezone-selector").classList.remove("open"); // Hide the dropdown
+};
+
+// Function to update the time display
+function updateTime(timezone) {
+    const now = dayjs().tz( selectedTimezone);
+
+    document.getElementById("day").textContent = now.format("dddd");
+    document.getElementById("month").textContent = now.format("MMMM");
+    document.getElementById("daynum").textContent = now.format("DD");
+    document.getElementById("year").textContent = now.format("YYYY");
+    document.getElementById("hour").textContent = now.format("HH");
+    document.getElementById("minute").textContent = now.format("mm");
+    document.getElementById("second").textContent = now.format("ss");
+    document.getElementById("period").textContent = now.format("A");
+}
+
+// Initial Time Display
+updateTime(); // Default timezone is UTC
+setInterval( () => updateTime() , 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function updateClock(){
     var now = new Date();
     var dname = now.getDay(),
     mo = now.getMonth(),    
@@ -38,5 +90,5 @@ function showTime(){
     updateClock();
      window.setInterval("updateClock()", 1);
  
- }
+ }*/
  
